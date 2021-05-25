78cb4c9e315b9ea8a8044cbb65de439f161dfdd5
stscoundrel <sampo@pixels.fi>
2021-05-25 18:40:08 +0300

    Dont add abbr tags inside html tags.
    Fixes #7

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
+const createRegexFor = (abbreviation) => {
+  const escapedAbbreviation = abbreviation.replace('.', '\\.');
+  return new RegExp(`(${escapedAbbreviation})(?![^<>]*<\/)`, 'gi'); // eslint-disable-line
+};
+
 export const abbreviate = (abbreviation: string, explanation: string, content: string): string => {
   if (abbreviation === undefined || explanation === undefined || content === undefined) throw new Error('Missing argument: expects three strings as function arguments');
 
@@ -5,7 +10,8 @@ export const abbreviate = (abbreviation: string, explanation: string, content: s
     return content;
   }
 
-  return content.split(abbreviation).join(`<abbr title="${explanation}">${abbreviation}</abbr>`);
+  const regex = createRegexFor(abbreviation);
+  return content.replace(regex, `<abbr title="${explanation}">${abbreviation}</abbr>`);
 };
 
 export default {
